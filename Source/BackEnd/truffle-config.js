fedf7d367984607452a4191afa63532cb0822de1
Maha Saad <maha.saad@symbyo.com>
2018-03-19 12:11:33 +0200

    Add other test net configurations

--- Source/BackEnd/truffle-config.js
+++ Source/BackEnd/truffle-config.js
@@ -1,4 +1,24 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "opinion destroy betray ..."; // 12 word mnemonic
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*" // Match any network id
+        },
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/");
+            },
+            network_id: '3',
+        },
+        // test: {
+        //     provider: function() {
+        //         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+        //     },
+        //     network_id: '*',
+        // },
+    }
 };
