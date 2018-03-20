e852a5068bf54f819001e74c8ce1055a752e8c7f
Maha Saad <maha.saad@symbyo.com>
2018-03-21 00:37:31 +0200

    add ACLHelper

--- Source/Front End/phr-manager/src/containers/PatientViewer.js
+++ Source/Front End/phr-manager/src/containers/PatientViewer.js
@@ -6,29 +6,29 @@ import * as EncryptionHelper from '../utils/EncryptionHelper'
 import * as StorageHelper from '../utils/StorageHelper'
 import * as PHRHelper from '../utils/PHRSmartContractHelper'
 import * as EthHelper from '../utils/EtherumHelper'
+import * as ACLHelper from '../utils/ACLHelper'
 
 class PatientViewer extends Component {
     uploadFile = (e) => {
-
-        debugger;
         var reader = new FileReader();
         reader.readAsText(e.target.files[0]);
         var plainFile = reader.result;
-        
+
         var sKey = EncryptionHelper.generateSharedKey();
         var encryptedFile = EncryptionHelper.encrypt(plainFile, sKey);
         var fileAddress = StorageHelper.uploadFile(encryptedFile);
 
         var pubKey = EthHelper.getCurrentAccount();
-        var encryptedSharedKey =  EncryptionHelper.encrypt(sKey, pubKey);
+        var encryptedSharedKey = EncryptionHelper.encrypt(sKey, pubKey);
         PHRHelper.addFileAccess(fileAddress, encryptedSharedKey);
 
         //var fileName = e.target.files[0].name;
     }
 
     render() {
+        ACLHelper.test();
         return (
-           <input type="file" onChange={this.uploadFile} />
+            <input type="file" onChange={this.uploadFile} />
         );
     }
 }
---
+++ Source/Front End/phr-manager/src/utils/ACLHelper.js
@@ -0,0 +1,128 @@
+
+
+function validateACLStructure(aclJson) {
+    if (!aclJson.hasOwnProperty('acl')) {
+        aclJson.acl = {};
+    }
+    if (!aclJson.acl.hasOwnProperty('publicAddress')) {
+        aclJson.acl.publicAddress = '';
+    }
+    if (!aclJson.acl.hasOwnProperty('files')) {
+        aclJson.acl.files = [];
+    }
+    if (!aclJson.acl.hasOwnProperty('shares')) {
+        aclJson.acl.shares = {};
+    }
+
+}
+export function addFileAccess(aclJson, myPubAddress, fileAddress, encryptedsharedkey, encryptedFileName) {
+
+    validateACLStructure(aclJson);
+
+    aclJson.acl.publicAddress = myPubAddress;
+    aclJson.acl.files.push({
+        'fileAddress': fileAddress,
+        'encryptedSymmetricKey': encryptedsharedkey,
+        'encryptedFileName': encryptedFileName,
+    })
+}
+
+
+
+export function shareFile(aclJson, recieverPubKey, fileAddress, encryptedsharedkey, encryptedFileName) {
+    validateACLStructure(aclJson);
+
+    if (!aclJson.acl.shares.hasOwnProperty(recieverPubKey)) {
+        aclJson.acl.shares[recieverPubKey] = [];
+    }
+
+    aclJson.acl.shares[recieverPubKey].push({
+        'fileAddress': fileAddress,
+        'encryptedSymmetricKey': encryptedsharedkey,
+        'encryptedFileName': encryptedFileName,
+    })
+}
+
+export function getMyFileAccess(aclJson, fileAddress) { //returns(string
+    var files = aclJson.acl.files;
+    if (!files)
+        return null;
+
+    return files.filter(file => file.fileAddress === fileAddress)[0];
+}
+
+export function getSharedFileAccess(aclJson, fileAddress, recieverPubKey) { //returns(string)
+    var files = aclJson.acl.shares[recieverPubKey];
+    if (!files)
+        return null;
+
+    return files.filter(file => file.fileAddress === fileAddress)[0];
+}
+
+export function listMyFiles(aclJson) {
+    return aclJson.acl.files;
+}
+
+export function listSharediles(aclJson, recieverPubKey) {
+    return aclJson.acl.shares[recieverPubKey];
+}
+
+
+//Test///////////////////////////////////////////////
+var aclFile =
+    {
+        "acl": {
+            "publicAddress": "Mahmoud Public Key",
+            "files": [
+                {
+                    "fileAddress": "<IPFS fileA address>",
+                    "encryptedSymmetricKey": "11111111111111111111",
+                    "encryptedFileName": "AAAAAAAAAAAAAAAAAAAAAA"
+                }, {
+                    "fileAddress": "<IPFS fileB address>",
+                    "encryptedSymmetricKey": "22222222222222222222222222",
+                    "encryptedFileName": "BBBBBBBBBBBBBBBBBBBBBBBBBBB"
+                }
+            ],
+            "shares": {
+                "Maha Public Key": [
+                    {
+                        "fileAddress": "<IPFS fileA address>",
+                        "encryptedSymmetricKey": "1X1X1X1X1X1X1X1X1X",
+                        "encryptedFileName": "AXAXAXAXAXAXAXAXAXAXAX"
+                    }, {
+                        "fileAddress": "<IPFS fileB address>",
+                        "encryptedSymmetricKey": "2X2X2X2X2X2X2X2X2X",
+                        "encryptedFileName": "BXBXBXBXBXBXBXBXBXBXBX"
+                    }
+                ],
+                "Taher Public Key": [
+                    {
+                        "fileAddress": "<IPFS fileA address>",
+                        "encryptedSymmetricKey": "1111111111111XXXXXXXXXXXXXX",
+                        "encryptedFileName": "AAAAAAAAAAAAAXXXXXXXXXXXXXXX"
+                    }
+                ]
+            }
+        }
+    }
+export function test() {
+    debugger;
+    var aclJson = {};
+    addFileAccess(aclJson, "Mahmoud Public Key", "<IPFS fileA address>", "11111111111111111111", "AAAAAAAAAAAAAAAAAAAAAA");
+    addFileAccess(aclJson, "Mahmoud Public Key", "<IPFS fileB address>", "22222222222222222222222222", "BBBBBBBBBBBBBBBBBBBBBBBBBBB");
+    var file = getMyFileAccess(aclJson, "<IPFS fileB address>");
+
+    shareFile(aclJson, "Maha Public Key", "<IPFS fileA address>", "1X1X1X1X1X1X1X1X1X", "AXAXAXAXAXAXAXAXAXAXAX");
+    shareFile(aclJson, "Maha Public Key", "<IPFS fileB address>", "2X2X2X2X2X2X2X2X2X", "BXBXBXBXBXBXBXBXBXBXBX");
+    var mahaFile = getSharedFileAccess(aclJson, "<IPFS fileB address>", "Maha Public Key")
+    var mahaFileFail = getSharedFileAccess(aclJson, "<IPFS fileB address>", "Maha Public Keysssssssssss")
+
+    shareFile(aclJson, "Taher Public Key", "<IPFS fileA address>", "1111111111111XXXXXXXXXXXXXX", "AAAAAAAAAAAAAXXXXXXXXXXXXXXX");
+    var taherFile = getSharedFileAccess(aclJson, "<IPFS fileA address>", "Taher Public Key")
+    var taherFileFail = getSharedFileAccess(aclJson, "<IPFS fileA addressvvvvvvvvvvvvv>", "Taher Public Key")
+
+    var myFiles = listMyFiles(aclJson);
+    var mahaFiles = listMyFiles(aclJson, "Maha Public Key");
+    var success = JSON.stringify(aclFile) === JSON.stringify(aclJson);
+}
