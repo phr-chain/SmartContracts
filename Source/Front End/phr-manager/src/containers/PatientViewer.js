b29c01c1ecc0d020b5b993fed93f1c1db420fd4d
TELHOOSIN-LAB-D\taher <taher.elhossin@symbyo.com>
2018-03-20 10:22:50 +0200

    revert

--- Source/Front End/phr-manager/src/containers/PatientViewer.js
+++ Source/Front End/phr-manager/src/containers/PatientViewer.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
 import { Row, Col, Button } from 'antd'
 import 'antd/lib/button/style/css';
-var ipfsAPI = require('ipfs-api')
 
 class PatientViewer extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            ipfsURL: ''
-        };
-    }
-
     uploadFile = (e) => {
-        var ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'}) 
-
-    var fileReader = new FileReader();
-    fileReader.readAsArrayBuffer(e.target.files[0]);
-  //  var fileData = fileReader.result;
-
-    fileReader.onload =  () => {
-      var data = fileReader.result
-      var buffer = Buffer.from(data)
-      var content = []
-      content.push({
-        path: "file 1",
-        content: buffer
-      })
-
-      ipfs.files.add(content, (err, res) => {
-        this.setState({ipfsURL:`https://gateway.ipfs.io/ipfs/${res[0].hash}`, fileName:res[0].path});
-        
-      });
-    }
+        var reader = new FileReader();
+        reader.readAsText(e.target.files[0]);
+        var fileData = reader.result;
 
     }
     render() {
@@ -40,11 +14,7 @@ class PatientViewer extends Component {
             <Row >
                 <Col>
                     <input type="file" onChange={this.uploadFile} />
-                    
                 </Col>
-                <div>
-                    <a href={this.state.ipfsURL}>{this.state.fileName}</a>
-                    </div>
             </Row>
         );
     }
