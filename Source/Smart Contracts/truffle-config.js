5579b6559a861d9b3f8cf9c859bd46585b6d38e9
Maha Saad <maha.saad@symbyo.com>
2018-03-20 10:56:40 +0200

    adjust truffle config

--- Source/Smart Contracts/truffle-config.js
+++ Source/Smart Contracts/truffle-config.js
@@ -1,24 +1,24 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "opinion destroy betray ..."; // 12 word mnemonic
+var mnemonic = "payment priority tool stand thought rug sail food rigid web hope soap"; 
 
 module.exports = {
-    networks: {
-        development: {
-            host: "127.0.0.1",
-            port: 8545,
-            network_id: "*" // Match any network id
-        },
-        ropsten: {
-            provider: function() {
-                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/");
-            },
-            network_id: '3',
-        },
-        // test: {
-        //     provider: function() {
-        //         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
-        //     },
-        //     network_id: '*',
-        // },
-    }
+    // networks: {
+    //     development: {
+    //         host: "127.0.0.1",
+    //         port: 8545,
+    //         network_id: "*" // Match any network id
+    //     },
+    //     ropsten: {
+    //         provider: function() {
+    //             return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/");
+    //         },
+    //         network_id: '3',
+    //     },
+    //     test: {
+    //         provider: function() {
+    //             return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+    //         },
+    //         network_id: '*',
+    //     },
+    // }
 };
--- Source/Smart Contracts/truffle.js
+++ Source/Smart Contracts/truffle.js
@@ -1,4 +1,24 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "payment priority tool stand thought rug sail food rigid web hope soap"; 
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*" // Match any network id
+        },
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/");
+            },
+            network_id: '3',
+        },
+        test: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+            },
+            network_id: '*',
+        },
+    }
 };
