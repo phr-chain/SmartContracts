9c85f16f4409a810d7189fba1a5424c86b27f30a
Mahmoud Samy <msamy.earth@gmail.com>
2018-03-20 22:05:56 +0200

    Implement EncryptionHelper

--- Source/Front End/phr-manager/src/utils/EncryptionHelper.js
+++ Source/Front End/phr-manager/src/utils/EncryptionHelper.js
@@ -1,10 +1,14 @@
+var CryptoJS = require("crypto-js");
+
 export function generateSharedKey(){
-     return "SharedKey";
+     return "TODO";
 }
-export function encrypt(plainText, key){
-     return "The encrypted file";
+export function encrypt(plainText, symmetricKey){
+    var encrypted  =  CryptoJS.AES.encrypt(plainText, symmetricKey);
+    return encrypted.toString();
 }
 
-export function decrypt(encryptedText, key){
-     return "The plain file";
+export function decrypt(encryptedText, symmetricKey){
+    var decrypted = CryptoJS.AES.decrypt(encryptedText, symmetricKey);
+    return decrypted.toString(CryptoJS.enc.Utf8);
 }
