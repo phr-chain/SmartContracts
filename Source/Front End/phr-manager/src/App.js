ce394bfeb5e98ac2eab3aaebf9202caf62d34835
Maha Saad <maha.saad@symbyo.com>
2018-03-21 14:53:12 +0200

    Switch between viewers

--- Source/Front End/phr-manager/src/App.js
+++ Source/Front End/phr-manager/src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 
-//import PatientViewer from'./containers/PatientViewer'
-import LoginViewer from'./containers/LoginViewer'
+import PatientViewer from'./containers/PatientViewer'
+import LoginViewer from './containers/LoginViewer'
 
 
 class App extends Component {
-  constructor(){
+  loginView = 0;
+  dashBoardView = 1;
+
+  constructor() {
     super();
     this.state = {
-      privateKey:'',
-      publicKey: ''
+      privateKey: '',
+      publicKey: '',
+      currentView: this.loginView,
     }
   }
   render() {
-    return (
+    if (this.state.currentView == this.loginView) {
+      return (
         <LoginViewer
-         privateKey={this.state.privateKey}
-         publicKey = {this.state.publicKey}
-         onPrivateKeyChanged = {key => this.setState({privateKey: key})}
-         onPublicKeyChanged = {key => this.setState({publicKey: key})}
-         />
-    );
+          privateKey={this.state.privateKey}
+          publicKey={this.state.publicKey}
+          onPrivateKeyChanged={key => this.setState({ privateKey: key })}
+          onPublicKeyChanged={key => this.setState({ publicKey: key })}
+          onLogin={() => this.setState({ currentView: this.dashBoardView })}
+        />
+      );
+    } else if (this.state.logincurrentView == this.dashBoardView) {
+
+      return (
+        <PatientViewer/>
+      );
+    } else {
+      return <div></div>
+    }
+
   }
 }
 
