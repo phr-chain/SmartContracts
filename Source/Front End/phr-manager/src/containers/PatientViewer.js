7b679271022d533e50c6dfdf4115bf9e1c76cf5b
Mahmoud Samy <msamy.earth@gmail.com>
2018-03-21 06:40:05 +0200

    - working version of file save - Known Issue:  * Enc/Dec text files only

--- Source/Front End/phr-manager/src/containers/PatientViewer.js
+++ Source/Front End/phr-manager/src/containers/PatientViewer.js
@@ -6,9 +6,8 @@ import * as EncryptionHelper from '../utils/EncryptionHelper'
 import * as StorageHelper from '../utils/StorageHelper'
 import * as PHRHelper from '../utils/PHRSmartContractHelper'
 import * as EthHelper from '../utils/EtherumHelper'
-import {saveAs} from 'file-saver';
 import * as ACLHelper from '../utils/ACLHelper'
-var FileSaver = require('file-saver');
+import saveAs from 'save-as'
 const CryptoJS = require("crypto-js");
 
 class PatientViewer extends Component {
@@ -21,30 +20,16 @@ class PatientViewer extends Component {
             return function(evt) {
                 var fileAsUtf8Text = reader.result;
                 var fileName = file.name;
+                var fileType = file.type;
                 var symmetricKey = EncryptionHelper.generateSharedKey();
                 var encryptedText = EncryptionHelper.encrypt(fileAsUtf8Text, symmetricKey);
-                var original = EncryptionHelper.decrypt(encryptedText, symmetricKey);
-                // var blob = new Blob([original], {type: "text/plain;charset=utf-8"});
-                // FileSaver.saveAs(blob, "encrypted-hello.txt");
-                var new_file = new File([original], "encrypted-world.txt", {type: "text/plain;charset=utf-8"});
-                
-                try{
-                    window.saveAs(new_file);
-                } catch(e){
-                    console.log("222222222222", e);
-                }
-                try{
-                    saveAs(new_file);
-                } catch(e){
-                    console.log("3333333333333", e);
-                }
-                console.log("original", original);
+                var decrypted = EncryptionHelper.decrypt(encryptedText, symmetricKey);
+                var blob = new Blob([decrypted], {type: fileType});
+                saveAs(blob, 'decrypted-'+fileName)
                 
                 // var blob1 = new Blob([byteArray], {type: "application/octet-stream"});
                 //var fileAddress = StorageHelper.uploadFile(encryptedFile);
-
-
-
+                
                 // var pubKey = EthHelper.getCurrentAccount();
                 // var encryptedSharedKey =  EncryptionHelper.encrypt(symmetricKey, pubKey);
                 //PHRHelper.addFileAccess(fileAddress, encryptedSharedKey);
