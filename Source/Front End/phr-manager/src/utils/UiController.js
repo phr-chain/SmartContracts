db8b6b4a1f3cedf327c4e3a80883fffd66b2cc8c
TELHOOSIN-LAB-D\taher <taher.elhossin@symbyo.com>
2018-03-22 19:40:40 +0200

    finish upload file

--- Source/Front End/phr-manager/src/utils/UiController.js
+++ Source/Front End/phr-manager/src/utils/UiController.js
@@ -1,8 +1,9 @@
 import * as StorageHelper from "./StorageHelper";
 import * as EncryptionHelper from "./EncryptionHelper"
 import * as ACLHelper from "./ACLHelper"
+import * as PHRSmartContractHelper from "./PHRSmartContractHelper"
 
-export function uploadFileToAccount(file, accountPublicAddress) {
+export function uploadFileToAccount(file, accountPublicAddress, currentACL) {
 
     var reader = new FileReader();
     return new Promise((resolve, reject) => {
@@ -19,16 +20,39 @@ export function uploadFileToAccount(file, accountPublicAddress) {
                 var genSymmetricKey = EncryptionHelper.generateSharedKey();
                 var cipher = EncryptionHelper.encrypt(arrayBuffer, genSymmetricKey);
 
+                // upoade file 
                 StorageHelper.uploadFile(cipher)
-                    .then((hashRes) => {
+                    .then((fileHash) => {
+                        // add file access
                         let fileAccess = {
-                            fileAddress: hashRes,
+                            fileAddress: fileHash,
                             symmetricKey: genSymmetricKey,
                             fileName: fileName,
                             size: fileLength,
                             mimeType: fileType
                         };
-                        ACLHelper.addFileAccess(fileAccess, accountPublicAddress)
+                        ACLHelper.addFileAccess(currentACL, accountPublicAddress, fileAccess);
+
+                        // store ACL in ipfs(encryption included) 
+                        StorageHelper.uploadAclFile(currentACL, accountPublicAddress)
+                            .then((aclHash) => {
+                                PHRSmartContractHelper.setACLFileAddress(aclHash, (smartContractError, smartContractResult) => {
+                                    if (error) {
+                                        reject(smartContractError);
+                                    } else {
+
+                                        resolve({
+                                            currentACL,
+                                            fileHash,
+                                            aclHash
+
+                                        });
+                                    }
+                                })
+                            })
+                            .catch((aclStoreError) => {
+                                reject(aclStoreError);
+                            });
                     })
                     .catch((uploadErr) => {
                         reject(uploadErr);
