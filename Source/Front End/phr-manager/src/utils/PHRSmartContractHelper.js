e27881d77c1b63eb72d6bc6017421420156dcfaf
Maha Saad <maha.saad@symbyo.com>
2018-03-22 17:55:09 +0200

    use deployed contract

--- Source/Front End/phr-manager/src/utils/PHRSmartContractHelper.js
+++ Source/Front End/phr-manager/src/utils/PHRSmartContractHelper.js
@@ -1,81 +1,96 @@
 import * as EthHelper from './EtherumHelper'
 
 var abi = [
-  {
-    "inputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "fallback"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "name": "fileOwner",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "name": "fileAddress",
-        "type": "address"
-      }
-    ],
-    "name": "FileAdded",
-    "type": "event"
-  },
-  {
-    "constant": false,
-    "inputs": [
-      {
-        "name": "_fileAddress",
-        "type": "address"
-      }
-    ],
-    "name": "setACLFileAddress",
-    "outputs": [],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "name": "_fileOwner",
-        "type": "address"
-      }
-    ],
-    "name": "getACLFileAddress",
-    "outputs": [
-      {
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
-];
-var address = '0x';
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "fileOwner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "fileAddress",
+				"type": "address"
+			}
+		],
+		"name": "FileAdded",
+		"type": "event"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_fileAddress",
+				"type": "address"
+			}
+		],
+		"name": "setACLFileAddress",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_fileOwner",
+				"type": "address"
+			}
+		],
+		"name": "getACLFileAddress",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getMyACLFileAddress",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+var address = '0x770e62cab50f942447b7ed04f75a9a4be98ea4e6';
+
 var phrContract = EthHelper.getContract(abi, address);
 
 
-function setACLFileAddress(fileAddress, callBack) {
-  var fees = window.web3.toWei(0.05, 'ether');
+export function setACLFileAddress(fileAddress, callBack) {
+  var fees = window.web3.toWei(0.06, 'ether');
   phrContract.setACLFileAddress(fileAddress, { value: fees, gas: 500000 }, callBack);
 }
 
-function getACLFileAddress(fileOwner, callBack) {
+export function getACLFileAddress(fileOwner, callBack) {
   phrContract.getACLFileAddress(fileOwner, callBack);
 }
 
-function getMyACLFileAddress(callBack) {
-  phrContract.getACLFileAddress(callBack);
+export function getMyACLFileAddress(callBack) {
+  phrContract.getMyACLFileAddress(callBack);
 }
